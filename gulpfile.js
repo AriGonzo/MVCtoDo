4a72729261799dbefe3d82b8f64cf283613883b5
Logan Arnett <logan@loganarnett.com>
2014-11-12 20:28:30 -0500

    toying with no todos and beginning edit

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,7 @@
 /*gulpfile with livereload */
 var gulp = require('gulp'),
     connect = require('gulp-connect');
-    wiredep = require('wiredep');
 
-gulp.task('wiredep', function(){
-  wiredep({src: 'index.html' });
-});
-
-gulp.task('bower', function(){
-  gulp.watch('bower.json', ['wiredep']);
-});
-
-gulp.task('connect', function() {
-  connect.server({
-    livereload: true
-  });
-});
 
 gulp.task('html', function () {
   gulp.src('./*.html')
@@ -26,4 +12,4 @@ gulp.task('watch', function () {
   gulp.watch(['./*.html'], ['html']);
 });
 
-gulp.task('default', ['connect', 'watch', 'bower']);
+gulp.task('default', ['connect', 'watch']);
